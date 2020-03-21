e21ccfb4d03ffd4e78d7d4e20b8a805026d5475c
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2020-03-21 20:11:21 +0530

    Removes h1 and h4 tags from nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,11 +7,9 @@ import Navigation, { NavigationProps } from "../navigation/navigation"
 const NavigationBrand = () => {
   return (
     <Navbar.Brand>
-      <h2>
-        <Link to="/" className="text-white bg-dark nav-link">
-          {"um"}
-        </Link>
-      </h2>
+      <Link to="/" className="text-white bg-dark nav-link h2 font-weight-bold">
+        {"um"}
+      </Link>
     </Navbar.Brand>
   )
 }
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -14,15 +14,13 @@ export interface NavigationProps {
 const NavigationItem = ({ name, link }: NavigationItemProps) => {
   return (
     <Nav.Item>
-      <h4>
-        <Link
-          to={link}
-          className="nav-link"
-          activeClassName="nav-link active text-primary"
-        >
-          {name}
-        </Link>
-      </h4>
+      <Link
+        to={link}
+        className="nav-link h4 font-weight-bold"
+        activeClassName="nav-link active text-primary h4 font-weight-bold"
+      >
+        {name}
+      </Link>
     </Nav.Item>
   )
 }
