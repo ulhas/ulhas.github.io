61c09560e13aae1d14caad690dac3461654aded0
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2020-03-22 11:17:47 +0530

    Adds google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-161396216-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
