19b6604fcad2138358602d5058c30e8e8e35a255
Travis CI User <travis@example.org>
2020-03-22 13:37:07 +0000

    Updates

--- component---src-pages-blog-tsx-1b342880628ea8f654af.js
+++ component---src-pages-blog-tsx-1b342880628ea8f654af.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"1GPU":function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),l=t.n(n),c=t("B7F5"),r=t("z2p8"),s=t("Bz5h"),m=t("7vrA"),u=function(e){var a=e.className;return l.a.createElement("h1",{className:a},"what i've to say")},o=function(){return l.a.createElement(m.a,{className:"mb-5"},l.a.createElement(u,{className:"my-5"}))};a.default=function(){return l.a.createElement(s.a,null,l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Blog"}),l.a.createElement(o,null)))}}}]);
-//# sourceMappingURL=component---src-pages-blog-tsx-d81849d1b28afb523210.js.map
+//# sourceMappingURL=component---src-pages-blog-tsx-1b342880628ea8f654af.js.map
